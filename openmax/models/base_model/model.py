104995a1313f39076b453f5bd23326f18cadc191
Dennys Huber <dennys.huber@proton.me>
2023-12-02 16:11:24 +0100

    try to fix gpu issue

--- openmax/models/base_model/model.py
+++ openmax/models/base_model/model.py
@@ -102,6 +102,9 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512 * block.expansion, num_classes)
+        logger.info(
+            f"{' Model Architecture '.center(90, '#')}\n{self}\n{' Model Architecture End '.center(90, '#')}"
+        )
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
